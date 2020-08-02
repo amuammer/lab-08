660f1a9eadc3121a6c96aeb8811c0671bd3b5bad
amuammer <amuammer@students.iugaza.edu.ps>
2020-08-02 14:36:18 +0300

    refactoring locationDB

--- Controllers/location/getLocation.js
+++ Controllers/location/getLocation.js
@@ -1,22 +1,21 @@
 const axios = require("axios");
 const Location = require("../../Models/Location").default;
 
-const client = global.DBclient;
-
 exports.default = (req, res, next) => {
   const { city } = req.query;
   try {
     if (!city) throw new Error();
     else if (!isNaN(city)) throw new Error();
-    getDBLocation(city, function (locationData) {
-
-      if (locationData) {
+    Location.findByName(city, function (result) {
+      if (result) {
+        let locationData = new Location(city, result.rows);
         res.status(200).send(locationData);
-      }
-      else {
-        getData(city, (locationItem) => {
-          addLocationToDB(locationItem);
-          res.status(200).send(locationItem);
+      } else {
+        getAPIData(city, (apiData) => {
+          const newLocation = new Location(city, apiData.data);
+          newLocation.save(() => {
+            res.status(200).send(newLocation);
+          });
         });
       }
     })
@@ -26,41 +25,10 @@ exports.default = (req, res, next) => {
 };
 
 
-function getData(city, callback) {
+function getAPIData(city, callback) {
   let LOCATIONAPIKEY = process.env.LOCATIONAPIKEY;
   let url = `https://eu1.locationiq.com/v1/autocomplete.php?key=${LOCATIONAPIKEY}&q=${city}&format=json`;
   axios.get(url).then(response => {
-    callback(new Location(city, response.data));
+    callback(response);
   });
-}
-
-
-function getDBLocation(city, callback) {
-  const SQL = `SELECT * FROM locations WHERE search_query=$1;`;
-  const values = [city];
-  client.query(SQL, values)
-    .then(result => {
-      // Check to see if the location was found and return the results
-      console.log("result.rowCount",result.rowCount);
-      if (result.rowCount > 0) {
-        console.log("From DB");
-        console.log("result.rows[0]", result.rows[0]);
-
-        let locationData = new Location(city, result.rows);
-        callback(locationData);
-        // Otherwise get the location information from the Google API
-      } else {
-        callback(null);
-      }
-    });
-}
-
-function addLocationToDB(locationData) {
-  const SQL = `INSERT INTO locations (search_query,display_name,lat,lon, country_code) VALUES ('${locationData.search_query}','${locationData.formatted_query}','${locationData.latitude}','${locationData.longitude}', '${locationData.countryCode}');`;
-  console.log("SQL: ", SQL);
-  client.query(SQL)
-    .then(result => { // eslint-disable-line
-      // console.log("addLocationToDB result: ", result);
-      // Check to see if the location was found and return the results
-    });
 }
